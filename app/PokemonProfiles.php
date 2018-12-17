a84983344c5e7ff2daf10a3145d4873ece29039f
Christos Papoulas <pachristos@programize.com>
2018-12-17 22:17:09 +0200

    Add function for finding the king

--- app/PokemonProfiles.php
+++ app/PokemonProfiles.php
@@ -20,4 +20,33 @@ class PokemonProfiles extends Model
     {
         return $this->belongsTo(Pokemons::class);
     }
+
+    public static function findKing()
+    {
+        $pokemonProfiles = PokemonProfiles::all();
+
+        $score = 0;
+        $kingId = null;
+        foreach ($pokemonProfiles as $pokemonProfile) {
+            $total = 0;
+            $jsonProfile = json_decode($pokemonProfile->profile);
+
+            foreach ($jsonProfile->stats as $stat) {
+                $total += $stat->base_stat;
+            }
+
+            if ($score < $total) {
+                $score = $total;
+                $kingId = $pokemonProfile->pokemon_id;
+            }
+        }
+
+        if (! $kingId) {
+            return null;
+        }
+
+        $king = Pokemons::find($kingId);
+
+        return compact(['king', 'score']);
+    }
 }
